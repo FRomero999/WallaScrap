d35eb70429082b0f13e9b9a808cdfe045eb98c01
Paco Romero <f.romero.guillen@gmail.com>
2021-07-12 19:28:19 +0200

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 import requests
 import json
 from requests.structures import CaseInsensitiveDict
@@ -6,22 +6,45 @@ from pyjsonq import JsonQ
 import os
 
 app = Flask(__name__)
+app.secret_key = 'BAD_SECRET_KEY'
+max_page = 10
+keywords=None
 
-@app.route('/')
-def hello_world():
+@app.before_request
+def before_request_func():
+    return
 
-    keywords=["playmovil+medieval",
-              "playmobil+medieval",
-              "playmobil+knights",
-              "playmovil+caballeros",
-              "playmobil+castillos",
-              "famobil+medieval"
-             ]
+@app.route('/', methods=['GET', 'POST'])
+def main():
+
+    if "keywords" in session:
+        keywords = session["keywords"];
+    else:
+        keywords = ["playmobil+medieval"]
+        session["keywords"] = keywords
+
+    if request.method == 'POST':
+        print(request.form)
+        keywords=(request.form['keyword'].split(","))
+        session["keywords"]=keywords
+
+    return render_template('index.html', listado=",".join(keywords), keywords=keywords)
+
+
+@app.route('/results')
+def results():
+
     max_page = 10
     resultados = [];
 
+    if "keywords" in session:
+        keywords = session["keywords"];
+    else:
+        keywords = ["playmobil+medieval"]
+        session["keywords"] = keywords
 
     for keyword in keywords:
+        print(keyword)
         for page in range(0,max_page):
             url = "https://api.wallapop.com/api/v3/general/search?country_code=ES&density_type=20&filters_source=search_box&keywords="
             url = url + keyword +"&language=es_ES&latitude=36.71847&longitude=-4.41965&order=closest&search_id=a5e5f67b-a5c3-4f0d-ab94-d7203d7f09fd&start="
@@ -32,24 +55,19 @@ def hello_world():
             resp = requests.get(url, headers=headers)
             data = json.loads(resp.text)
 
-            print("pagina "+str(page))
             productos = len(data["search_objects"])
-            print(productos)
-            print(url)
+            print("    pagina "+str(page) +": "+str(productos))
 
             resultados = resultados + data["search_objects"]
 
             if productos<30:
                 break
 
-
     q=JsonQ(data=resultados)
     salida=q.sort_by('price').get()
 
-    print(salida)
-
-    return render_template('index.html', data=salida)
+    return render_template('results.html', data=salida, cantidad=len(salida), cantidad_claves=len(keywords))
 
 
 port = int(os.environ.get('PORT', 5000))
-server=app.run(host='0.0.0.0', port=port)
+server=app.run(host="localhost", port=port)
